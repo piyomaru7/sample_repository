5791e4034805f38b823eb8ad88f3c1b18396b232
piyomaru7 <kbtslugger14@gmail.com>
2023-05-30 15:48:32 +0900

    env.phpの作成

--- PHP応用_演習/Models/dbc.php
+++ PHP応用_演習/Models/dbc.php
@@ -1,9 +1,13 @@
 <?php
 
+require_once('env.php');
+
 function dbConnect() {
-  $dsn = 'mysql:host=localhost;dbname=casteria;charset=utf8';
-  $user = 'contacts_user';
-  $pass = 'Mzcin_1998';
+  $host   = DB_HOST;
+  $dbname = DB_NAME;
+  $user   = DB_USER;
+  $pass   =  DB_PATH;
+  $dsn    = "mysql:host=$host;dbname=$dbname;charset=utf8";
 
   try {
     $dbh = new PDO($dsn, $user, $pass,[
